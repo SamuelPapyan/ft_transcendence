0f3f08d78b789d7e5d12b56ba3878c1962cd7162
SamuelPapyan <samuel.papyan170@gmail.com>
2024-05-15 01:23:28 +0400

    feat: added server-side pong action v1

--- backend/consumers.py
+++ backend/consumers.py
@@ -14,9 +14,9 @@ class MatchMaking(WebsocketConsumer):
         print(MatchMaking.users)
         print(text_data, type(text_data))
         obj = json.loads(text_data)
-        if obj["user"] in list(MatchMaking.users.keys()):
-            self.close(1000)
         if obj["method"] == 'connect':
+            if obj["user"] in list(MatchMaking.users.keys()):
+                self.close(1000)
             MatchMaking.users[obj["user"]] = self
         elif obj["method"] == 'disconnect':
             del(MatchMaking.users[obj["user"]])
@@ -29,3 +29,106 @@ class MatchMaking(WebsocketConsumer):
     def disconnect(self, close_code):
         print("Disconnect")
         # Called when the socket closes
+
+class PongConsumer(WebsocketConsumer):
+    users = {}
+    data = {
+        "game_on": False,
+        "x": 0,
+        "y": 0,
+        "p1_score": 0,
+        "p2_score": 0,
+        "p1_y": 200,
+        "p2_y": 200,
+        "dx": -1,
+        "dy": -1,
+    }
+    def connect(self):
+        if len(PongConsumer.users) == 2:
+            self.disconnect(1000)
+        self.accept()
+
+    def reset_game_data(self):
+        PongConsumer.data = {
+            "game_on": False,
+            "x": 0,
+            "y": 0,
+            "p1_score": 0,
+            "p2_score": 0,
+            "p1_y": 200,
+            "p2_y": 200,
+            "dx": -1,
+            "dy": -1,
+        }
+    
+    def read_frame(self):
+        if (PongConsumer.data["game_on"]):
+            # P1 Goal
+            if (PongConsumer.data["x"] + 400 - 10 + PongConsumer.data["dx"]) > (800 - 10):
+                PongConsumer.data["p1_score"] += 1
+                PongConsumer.data["x"] = 0
+                PongConsumer.data["y"] = 0
+            
+            #P2 Goal
+            if (PongConsumer.data["x"] + 400 - 10 + PongConsumer.data["dx"]) < 10:
+                PongConsumer.data["p2_score"] += 1
+                PongConsumer.data["x"] = 0
+                PongConsumer.data["y"] = 0
+            
+            # Floor/Ceiling Collision
+            if (PongConsumer.data["y"] + 250 - 10 + PongConsumer.data["dy"] > 500 - 10) or (PongConsumer.data["y"] + 250 - 10 + PongConsumer.data["dy"] < 10):
+                PongConsumer.data["dy"] = -PongConsumer.data["dy"]
+            
+            # P1 Collision
+            if (PongConsumer.data["y"] + 250 - 10 >= PongConsumer.data["p1_y"]) and (PongConsumer.data["y"] + 250 - 10 <= PongConsumer.data["p1_y"] + 100 - 10) and (PongConsumer.data["x"] + 400 - 10 + PongConsumer.data["dx"] < 40 + 10):
+                PongConsumer.data["dx"] = -PongConsumer.data["dx"]
+            
+            # P2 Collision
+            if (PongConsumer.data["y"] + 250 - 10 >= PongConsumer.data["p2_y"]) and (PongConsumer.data["y"] + 250 - 10 <= PongConsumer.data["p2_y"] + 100 - 10) and (PongConsumer.data["x"] + 400 - 10 + PongConsumer.data["dx"] > 800 - 40 -10):
+                PongConsumer.data["dx"] = -PongConsumer.data["dx"]
+            
+            # Common Movement
+            PongConsumer.data["x"] += PongConsumer.data["dx"]
+            PongConsumer.data["y"] += PongConsumer.data["dy"]
+
+    def receive(self, text_data=None, bytes_data=None):
+        obj = json.loads(text_data)
+        winner = None
+        final_scores = [0, 0]
+        if obj["method"] == 'connect':
+            if obj["user"] in list(PongConsumer.users.keys()):
+                self.close(1000)
+            PongConsumer.users[obj["user"]] = self
+            if len(PongConsumer.users) == 2:
+                PongConsumer.data["game_on"] = True
+        elif obj["method"] == 'disconnect':
+            del(PongConsumer.users[obj["user"]])
+            PongConsumer.data["game_on"] = False
+        elif obj["method"] in ['game', 'move']:
+            if obj["method"] == 'move':
+                if PongConsumer.data["game_on"] and obj["user"] in list(PongConsumer.users.keys()):
+                    if list(PongConsumer.users.keys()).index(obj["user"]) == 0:
+                        PongConsumer.data["p1_y"] += obj["movement"]
+                    elif list(PongConsumer.users.keys()).index(obj["user"]) == 1:
+                        PongConsumer.data["p2_y"] += obj["movement"]
+            self.read_frame()
+        final_scores = [PongConsumer.data["p1_score"], PongConsumer.data["p2_score"]]
+        if (PongConsumer.data["p1_score"] == 10 or PongConsumer.data["p2_score"] == 10):
+                PongConsumer.data["game_on"] = False
+                ps = list(PongConsumer.users.keys())
+                if PongConsumer.data["p1_score"] == 10:
+                    winner = ps[0]
+                else:
+                    winner = ps[1]
+                self.reset_game_data()
+        for i in PongConsumer.users:
+            PongConsumer.users[i].send(text_data=json.dumps({
+                **obj,
+                "winner": winner,
+                "final_scores": final_scores,
+                "game_data": PongConsumer.data,
+                "members": list(PongConsumer.users.keys())
+            }))
+    
+    def disconnect(self, code):
+        print("Disconnect from Pong")
--- frontend/static/js/client/PongClient.js
+++ frontend/static/js/client/PongClient.js
@@ -1,7 +1,7 @@
-class MatchMakingClient {
+class PongClient {
     constructor(client) {
         this.client = client;
-        this.searching = false;
+        this.onGame = false;
         this.user = "";
     }
 
@@ -9,13 +9,13 @@ class MatchMakingClient {
         this._socket.send(JSON.stringify({
             "method": "connect",
             "user": this.user
-        }));
+        }))
     }
 
     async start(receive_func, disconnect_func, username) {
-        console.log("start");
+        console.log("start pong");
         this.user = username;
-        let url = `ws://localhost:8000/ws`;
+        let url = `ws://localhost:8000/ws/pong`;
 
         this._socket = new WebSocket(url);
 
@@ -34,26 +34,43 @@ class MatchMakingClient {
         window.addEventListener('beforeunload', this.clientLeave.bind(this));
     }
 
+    sendMove(movement) {
+        this._socket.send(JSON.stringify({
+            "method": "move",
+            "user": this.user,
+            "movement": movement
+        }))
+    }
+
+    sendGame() {
+        this._socket.send(JSON.stringify({
+            "method": "game",
+            "user": this.user
+        }))
+    }
+
     onclose(event) {
+        console.log(this.user, "closing with event handler")
+        console.log(event);
         this.stop();
         this.disconnect_func(event);
     }
 
-    clientLeave(){
-        if (this._socket) {
-            this._socket.send(JSON.stringify({
-                "method": "disconnect",
-                "user": this.user
-            }));
-        }
+    clientLeave() {
+        console.log(this.user, "pong client leaving");
+        this._socket.send(JSON.stringify({
+            "method": "disconnect",
+            "user": this.user
+        }));
     }
 
     stop(){
+        console.log(this.user, "pong client stopping");
         if (this._socket)
             this._socket.close()
         this._socket = undefined;
-        this.searching = false;
+        this.onGame = false;
     }
 }
 
-export {MatchMakingClient}
+export {PongClient}
